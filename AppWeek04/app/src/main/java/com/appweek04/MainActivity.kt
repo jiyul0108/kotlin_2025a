d957df8bead65da227198b39482abe4188723e72
jiyul <rlawldbf0108@gmail.com>
2025-09-26 12:49:15 +0900

    week04 : Greeting App v0.2

--- AppWeek04/app/src/main/java/com/appweek04/MainActivity.kt
+++ AppWeek04/app/src/main/java/com/appweek04/MainActivity.kt
@@ -1,37 +1,19 @@
 package com.appweek04
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editTextName = findViewById<EditText>(R.id.editTextName)
-        val buttonGreat = findViewById<Button>(R.id.buttonGreat)
-        val textViewGreating = findViewById<TextView>(R.id.textViewGreating)
+        val buttonColor = findViewById<Button>(R.id.buttonGreet)
 
-        buttonGreat.setOnClickListener {
-            val name = editTextName.text.toString().trim()
-
-            var greating: String = ""
-
-            if(name.isNotEmpty()){
-                greating = "안녕하세요, ${name}님!"
-
-            }else{
-                greating = "이름을 입력해주세요"
-            }
-            textViewGreating.text = greating
-            textViewGreating.visibility = View.VISIBLE
-            Log.d("kotlinWeek04App", greating)
+        buttonColor.setOnClickListener {
+            startActivity(Intent(this, GreetingActivity::class.java))
         }
     }
 }
